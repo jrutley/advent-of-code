c5d8b6f20aaca7f01f533897032d42fba1e32ffa
Jeramy Rutley <jrutley@gmail.com>
2020-12-12 13:29:24 -0500

    Refactor

--- 2020/day9/src/main.rs
+++ 2020/day9/src/main.rs
@@ -1,13 +1,20 @@
 fn main() {
     let file_data: Vec<i64> = textfilereader::read_file_by_line("input.txt").into_iter().map(|n| n.parse::<i64>().unwrap()).collect();
     let window_size = 25;
+    let invalid_number = get_invalid_number(file_data, window_size);
+    if invalid_number == None { println!("Couldn't find anomalous number"); return; }
+    let target_number = invalid_number.unwrap();
+    println!("{} is the first non-matching", target_number);
+}
+
+fn get_invalid_number(file_data: Vec<i64>, window_size: usize) -> Option<i64>{
     for numbers in file_data.windows(window_size + 1) {
         let target_number = numbers[window_size];
         if !did_match(target_number, &numbers[0..window_size], window_size-1) {
-            println!("{} is the first non-matching", target_number);
-            return;
+            return Some(target_number);
         }
     }
+    None
 }
 
 fn did_match(target_number: i64, numbers: &[i64], window_size: usize) -> bool {
