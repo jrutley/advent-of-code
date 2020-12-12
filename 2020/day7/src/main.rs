0dde538e5154fc02cbd0e84b4d25ee8453d8899c
Jeramy Rutley <jrutley@gmail.com>
2020-12-11 20:37:59 -0500

    Solution for day 7 part 2

--- 2020/day7/src/main.rs
+++ 2020/day7/src/main.rs
@@ -1,71 +1,65 @@
-use crate::trees::ArenaTree;
-use crate::trees::Node;
-use std::collections::HashSet;
+use std::collections::HashMap;
 use regex::Regex;
-
-mod trees;
-
-#[derive(PartialEq, Default)]
-struct Bag {
-    description: String,
-    colour: String
-}
 
 fn main() {
     let file_data = textfilereader::read_file_by_line("input.txt");
     let result = execute(file_data, "shiny gold");
-    println!("bag colours that eventually contain at least one shiny gold bag: {}", result.len());
+    println!("bag colours that eventually contain at least one shiny gold bag: {}", result);
 }
 
-fn execute(file_data: Vec<String>, search_text: &str) -> HashSet<usize> {
+/*
+input : [
+    ["muted lavender", " 5 dull brown bags, 4 pale maroon bags, 2 drab orange bags."],
+    ["plaid aqua", " 1 posh violet bag, 5 pale yellow bags, 4 bright salmon bags."]
+]
+output: {
+    muted lavender: [(dull brown, 5), (pale maroon, 4), (drab orange, 2)],
+    plaid aqua: [(posh violet, 1), (pale yellow, 5), (bright salmon, 4)]
+}
+*/
+fn split_contained(input: &Vec<Vec<String>>) -> HashMap<String, Vec<(String, i32)>>{
+    let child_regex = Regex::new(r"^ ?(\d+?)? (\w+ \w+) bag").unwrap();
+    let mut result = HashMap::default();
+    for i in input.iter() {
+        //[muted lavender, 5 dull brown bags, 4 pale maroon bags, 2 drab orange bags.]
+        let parent = i[0].to_owned();
+        let children_raw = i[1].split(", ").map(|j| j.to_string()).collect::<Vec<String>>();
+        let mut children = vec!();
+        for c in children_raw {
+            for cap in child_regex.captures_iter(&c) {
+                let s = cap.get(1);
+                if let Some(number) = s {
+                    let count = number.as_str().parse::<i32>().unwrap();
+                    children.push(((&cap[2]).to_string(), count));
+                }
+            }
+        }
+        result.insert(parent, children);
+    }
+    result
+}
+
+// file_data: [muted lavender bags contain 5 dull brown bags, 4 pale maroon bags, 2 drab orange bags.]
+fn execute(file_data: Vec<String>, search_text: &str) -> i32 {
     let input: Vec<Vec<String>> = file_data.iter().map(|i| i
         .split(" bags contain")
         .map(|m| m.to_string())
         .collect::<Vec<String>>()
     ).collect::<Vec<Vec<String>>>();
 
-    let mut tree: ArenaTree<String> = trees::ArenaTree::default();
+    let map: HashMap<String, Vec<(String, i32)>> = split_contained(&input);
 
-    insert_nodes(&mut tree, &input);
-    link_nodes(&mut tree, &input);
-
-    let search_node: usize = tree.node(search_text.to_string().into());
-    let mut parent_hash = HashSet::new();
-    let current_node = &tree.arena[search_node];
-    add_parents_to_hash(&mut parent_hash, &tree, &current_node, 0);
-    parent_hash
+    search_children(search_text, &map) - 1
 }
 
-fn add_parents_to_hash(containing_node_ids: &mut HashSet<usize>, tree: &ArenaTree<String>, node: &Node<String>, depth: i32){
-    if depth > 0 {
-        containing_node_ids.insert(node.idx);
+// map: "muted lavender: [(dull brown, 5), (pale maroon, 4), (drab orange, 2)]"
+fn search_children(search_text: &str, map: &HashMap<String, Vec<(String, i32)>>) -> i32 {
+    let mut results = 0;
+    for child in &map[search_text] {
+        let (child_text, count) = child;
+        results += count * search_children(&child_text, map);
     }
-    for p in node.parent.iter() {
-        let current_node = &tree.arena[*p];
-        add_parents_to_hash(containing_node_ids, tree, current_node, depth + 1);
-    }
-}
-
-fn link_nodes(tree: &mut trees::ArenaTree<String>, input: &Vec<Vec<String>>){
-    let child_regex = Regex::new(r"^ ?\d+ (\w+ \w+) bag").unwrap();
-    for i in input.iter() {
-        let parent = tree.node(i[0].to_owned().into());
-
-        let children = i[1].split(", ").map(|j| j.to_string()).collect::<Vec<String>>();
-        for c in children {
-            for cap in child_regex.captures_iter(&c) {
-                let child = tree.node((&cap[1]).to_string());
-                tree.link_parent_child(parent, child);
-            }
-        }
-    }
-}
-
-fn insert_nodes(tree: &mut trees::ArenaTree<String>, input: &Vec<Vec<String>>){
-    for i in input.iter() {
-        let first_half = i[0].to_owned();
-        tree.node(first_half.into());
-    }
+    1 + results
 }
 
 #[cfg(test)]
@@ -73,90 +67,17 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_sample_input(){
+    fn test_containing_bags(){
         let file_data: Vec<String> = vec!(
-            "light red bags contain 1 bright white bag, 2 muted yellow bags.".to_string(),
-            "dark orange bags contain 3 bright white bags, 4 muted yellow bags.".to_string(),
-            "bright white bags contain 1 shiny gold bag.".to_string(),
-            "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.".to_string(),
-            "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.".to_string(),
-            "dark olive bags contain 3 faded blue bags, 4 dotted black bags.".to_string(),
-            "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.".to_string(),
-            "faded blue bags contain no other bags.".to_string(),
-            "dotted black bags contain no other bags.".to_string()
+            "shiny gold bags contain 2 dark red bags.".to_string(),
+            "dark red bags contain 2 dark orange bags.".to_string(),
+            "dark orange bags contain 2 dark yellow bags.".to_string(),
+            "dark yellow bags contain 2 dark green bags.".to_string(),
+            "dark green bags contain 2 dark blue bags.".to_string(),
+            "dark blue bags contain 2 dark violet bags.".to_string(),
+            "dark violet bags contain no other bags.".to_string()
         );
         let result = execute(file_data, "shiny gold");
-        assert_eq!(result.len(), 4);
-    }
-
-    #[test]
-    fn test_direct_descendant_input(){
-        let file_data: Vec<String> = vec!(
-            "bright white bags contain 1 shiny gold bag.".to_string(),
-            "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.".to_string(),
-            "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.".to_string(),
-            "dark olive bags contain 3 faded blue bags, 4 dotted black bags.".to_string(),
-            "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.".to_string(),
-            "faded blue bags contain no other bags.".to_string(),
-            "dotted black bags contain no other bags.".to_string()
-        );
-        let result = execute(file_data, "shiny gold");
-        assert_eq!(result.len(), 2);
-    }
-
-    #[test]
-    fn test_group_single_line() {
-        // "muted lavender bags contain 5 dull brown bags, 4 pale maroon bags.".to_string(),
-        // "dull brown bags contain no other bags.".to_string(),
-        // "pale maroon bags contain no other bags.".to_string()
-
-        let input: Vec<Vec<String>> =
-            vec!(
-                vec!("muted lavender".to_string(), " 5 dull brown bags, 4 pale maroon bags.".to_string()),
-                vec!("dull brown".to_string(), " no other bags.".to_string()),
-                vec!("pale maroon".to_string(), " no other bags.".to_string())
-            );
-        let mut tree: trees::ArenaTree<String> = trees::ArenaTree::default();
-        insert_nodes(&mut tree, &input);
-        link_nodes(&mut tree, &input);
-
-        assert_eq!(tree.size(), 3);
-        let lavender = tree.node("muted lavender".into());
-        assert_eq!(tree.arena[lavender].children.len(), 2);
+        assert_eq!(result, 126);
     }
 }
-
-    //let x = "string".to_string().split("i").map(|i| i.to_string()).collect::<Vec<String>>();
-    // for i in input.iter() {
-    //     // let containing_bag = splits[0].split(' ').take(2).collect::<Vec<&str>>();
-    //     let container = Bag{ description: containing_bag[0].to_string(), colour: containing_bag[1].to_string()};
-    //     tree.node(container);
-    // }
-
-    // for i in input {
-    //     let splits: Vec<&str> = i.split(" bags contain").collect();
-    //     let contained_bag_list = splits[1].split(", ").collect::<Vec<&str>>();
-    //     let tokenized_contained_bags = contained_bag_list.iter().map(|b| b.split(" ").take(3).collect::<Vec<&str>>()).collect::<Vec<Vec<&str>>>();
-    // }
-
-
-    // let hello = tree.node("Hello".into());
-    // let world = tree.node("World".into());
-    // tree.link_parent_child(hello, world);
-    // let exclamation = tree.node("!".into());
-    // tree.link_parent_child(world, exclamation);
-    // let period = tree.node(".".into());
-    // let period_2 = tree.node(".".into());
-    // assert_eq!(period, period_2);
-    // tree.link_parent_child(world, period);
-    // // tree.arena[world].children.push(period);
-    // // tree.arena[period_2].parent = Some(world);
-
-    // println!(
-    //     "Total nodes: {}\nTotal edges: {}\nDepth of '.': {}\nDistance between World and !: {}",
-    //     tree.size(),
-    //     tree.edges(),
-    //     tree.depth(period),
-    //     tree.distance_between("World".into(), "!".into())
-    // );
-// }
