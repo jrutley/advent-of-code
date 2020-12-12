2387367308342afaac35867e858fe297e7a0e3d9
Jeramy Rutley <jrutley@gmail.com>
2020-12-12 12:18:33 -0500

    Solution for day 8 part 2

--- 2020/day8/src/main.rs
+++ 2020/day8/src/main.rs
@@ -1,11 +1,13 @@
 use std::collections::HashSet;
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone, PartialEq)]
 enum Instruction {
     NOP,
     ACC,
     JMP
 }
+
+#[derive(Copy, Clone)]
 struct Op {
     position: i32,
     instruction: Instruction,
@@ -23,6 +25,50 @@ impl Op {
 
 fn main() {
     let file_data = textfilereader::read_file_by_line("input.txt");
+    let input = generate_initial_instructions(file_data);
+
+    let mut twiddle_counter = 0;
+    while twiddle_counter < input.len() {
+        let new_instruction = match input[twiddle_counter].instruction {
+            Instruction::NOP => Instruction::JMP,
+            Instruction::ACC => Instruction::ACC,
+            Instruction::JMP => Instruction::NOP
+        };
+        if new_instruction == input[twiddle_counter].instruction {
+            twiddle_counter += 1;
+            continue;
+        }
+        // twiddle input
+        let mut twiddled_input: Vec<Op> = input.iter().map(|&i| i).collect();
+        twiddled_input[twiddle_counter].instruction = new_instruction;
+
+        let (success, acc) = run_simulation(&twiddled_input);
+
+        if success {
+            println!("Acc is {}", acc);
+            return;
+        }
+        twiddle_counter += 1;
+    }
+    panic!("Couldn't find the instruction!");
+}
+
+fn run_simulation(input: &Vec<Op>) -> (bool, i32) {
+    let mut current_instruction: i32 = 0;
+    let mut hash = HashSet::new();
+    let mut acc = 0;
+    let input_len = input.len() as i32;
+    loop {
+        if current_instruction == input_len {return (true, acc);}
+        if hash.contains(&current_instruction) {return (false, acc);}
+        let (next_op, delta) = input[current_instruction as usize].execute();
+        acc += delta;
+        hash.insert(current_instruction);
+        current_instruction = next_op;
+    }
+}
+
+fn generate_initial_instructions(file_data: Vec<String>) -> Vec<Op> {
     let mut counter = 0;
     let input: Vec<Op> = file_data.iter().map(|f| {
         let position = counter;
@@ -40,15 +86,5 @@ fn main() {
             value: input[1].parse::<i32>().unwrap()
         }
     }).collect();
-
-    let mut current_instruction: i32 = 0;
-    let mut hash = HashSet::new();
-    let mut acc = 0;
-    loop {
-        if hash.contains(&current_instruction) {println!("Acc is {}", acc); return;}
-        let (next_op, delta) = input[current_instruction as usize].execute();
-        acc += delta;
-        hash.insert(current_instruction);
-        current_instruction = next_op;
-    }
+    input
 }
