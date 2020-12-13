9cd8534fa3b20d4ca14c08c12d7b980cb1b8bc35
Jeramy Rutley <jrutley@gmail.com>
2020-12-12 19:11:22 -0500

    Solution for Day 10 part 1

--- 2020/day10/src/main.rs
+++ 2020/day10/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    let file_data = textfilereader::read_file_by_line("input.txt");
+    let mut jolts: Vec<i32> = textfilereader::read_file_by_line("input.txt").into_iter()
+        .map(|n| n.parse::<i32>().unwrap()).collect();
+    jolts.sort();
+
+    let (mut ones, mut threes) = (0, 1);
+    let mut current = 0;
+    for jolt in jolts {
+        if jolt - current == 1 {
+            ones += 1;
+        } else if jolt - current == 3 {
+            threes += 1;
+        }
+        current = jolt;
+    }
+    println!("Ones: {} Threes: {}", ones, threes);
+    println!("Multiplied: {}", ones * threes);
 }
